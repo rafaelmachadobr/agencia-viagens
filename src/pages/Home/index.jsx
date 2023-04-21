c37a9beacc5f7f41cf09529a1745f658f56c204a
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-21 13:41:59 -0300

    Componente Section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,18 @@
-import Header from '../../components/Header/Index';
-import styles from './styles.module.scss';
+import Header from "../../components/Header";
+import { Section } from "../../components/Section";
+import Mala from "../../assets/imagens/mala.jpg";
 
 export default function Home() {
   return (
     <>
-      <Header /> 
+      <Header />
+      <Section
+        titulo="Encontre aqui a viagem dos seus sonhos!!"
+        conteudo="lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Nullam auctor, nisl eget ultricies tincidunt, nunc elit
+        tincidunt nunc, eget aliquam massa nisl eget lorem. Sed"
+        imagem={Mala}
+      />
     </>
   );
 }
